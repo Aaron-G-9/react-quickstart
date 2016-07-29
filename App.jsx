046c09b0b3280bec66de22077c8fb3708b98691f
Aaron-G-9 <amgoodfellow@oakland.edu>
2016-07-29 01:14:50 -0400

    Initial Commit

---
+++ App.jsx
@@ -0,0 +1,12 @@
+class App extends React.Component{
+    render(){
+        return(
+            <div className="hello-world">
+              Hello World!
+            </div>
+        );
+    }
+}
+
+
+ReactDOM.render(<App />, document.getElementById('content'));
